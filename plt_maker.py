1bc4aabd2e5cda81dd86f1430bebeb2c73d56bc1
pasmoowsvd <ppasmo@gmail.com>
2023-01-28 01:34:25 +0900

    style css 파일 추가

--- plt_maker.py
+++ plt_maker.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 
 f = plt.figure()
 plt.plot([1,2,3], [4,5,6])
-plt.savefig('img_1.jpg', dpi = 50)
-plt.savefig('img_2.jpg')
-plt.savefig('img_3.jpg', dpi = 200)
+plt.savefig('image/img_1.jpg', dpi = 50)
+plt.savefig('image/img_2.jpg')
+plt.savefig('image/img_3.jpg', dpi = 100)
